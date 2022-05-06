aaf8decb4f45d5f092a7bccb67d6001928a2fcd6
NoobleMcScrunchkin <kieranaslett@gmail.com>
2022-05-06 18:52:26 +0100

    Fix?

--- index.js
+++ index.js
@@ -184,7 +184,7 @@ const job = schedule.scheduleJob('12 * * *', async () => {
 app.post('/eval', async (req, res) => {
     const AsyncFunction = Object.getPrototypeOf(async function () { }).constructor;
     try {
-        let output = await (new AsyncFunction(req.body.command)).bind({client})();
+        let output = await (new AsyncFunction('client', req.body.command))(client);
         res.json({
             success: true,
             output,
